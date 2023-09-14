81a928ea3fdfe0cfd1b36c6547222ac0b37bc4d1
BasmalaAyman4 <124400018+BasmalaAyman4@users.noreply.github.com>
2023-09-14 15:34:34 +0300

    add product dashboard

--- src/Dashboard/AddProduct/AddProduct.js
+++ src/Dashboard/AddProduct/AddProduct.js
@@ -1,10 +1,111 @@
-import React, { useEffect } from 'react'
-
-
+import React, { useEffect, useState } from 'react'
+import styles from './AddProduct.module.css'
+import { Col, Container, FormGroup, Row } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form';
+import { ToastContainer, toast } from 'react-toastify';
+import { db, storage } from '../../firebase.config'
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { collection, addDoc } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom';
 export default function AddProduct() {
+    const [title, setTitle] = useState('')
+    const [shortDesc, setshortDesc] = useState('')
+    const [description, setDescription] = useState('')
+    const [price, setPrice] = useState('')
+    const [category, setCategory] = useState('')
+    const [img, setImg] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+    const addProduct = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+
+        try {
+
+            const docRef = await collection(db, 'products')
+            const storeRef = ref(storage, `productImages/${Date.now() + img.name}`)
+            const uploadTask = uploadBytesResumable(storeRef, img)
+            uploadTask.on(() => {
+                toast.error('images not uploades')
+            }, () => {
+                getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                    await addDoc(docRef, {
+                        Title: title,
+                        ShortDesc: shortDesc,
+                        Description: description,
+                        Category: category,
+                        Price: price,
+                        imgUrl: downloadURL
+                    })
+                })
+
+            })
+            setLoading(false)
+            toast.success('Product successfully added')
+            navigate("/dashboard/all-product")
+        } catch (err) {
+            setLoading(false)
+            toast.error('product not added')
+
+        }
+
+    }
     return (
         <>
-            <div>ggg</div>
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='12'>
+                            {
+                                loading ? <h4>loading....</h4>
+                                    :
+                                    <>
+                                        <h4 className='mb-5'>Add Product</h4>
+                                        <Form onSubmit={addProduct}>
+                                            <Form.Group className={`${styles.form__group} mb-3`}>
+                                                <Form.Label>Product Title</Form.Label>
+                                                <Form.Control type='text' placeholder='Double sofa' value={title} onChange={e => setTitle(e.target.value)} />
+                                            </Form.Group>
+                                            <Form.Group className={`${styles.form__group} mb-3`}>
+                                                <Form.Label>Short Description</Form.Label>
+                                                <Form.Control type='text' placeholder='Double sofa' value={shortDesc} onChange={e => setshortDesc(e.target.value)} />
+                                            </Form.Group>
+                                            <Form.Group className={`${styles.form__group} mb-3`}>
+                                                <Form.Label>Description</Form.Label>
+                                                <Form.Control type='text' placeholder='Double sofa' value={description} onChange={e => setDescription(e.target.value)} />
+                                            </Form.Group>
+                                            <div className='d-flex align-items-center justify-content-between gap-5 mb-3'>
+                                                <Form.Group className={`${styles.form__group} w-50`}>
+                                                    <Form.Label>Price</Form.Label>
+                                                    <Form.Control type='number' placeholder='Double sofa' value={price} onChange={e => setPrice(e.target.value)} />
+                                                </Form.Group>
+                                                <Form.Group className={`${styles.form__group} w-50`}>
+                                                    <Form.Label>Category</Form.Label>
+                                                    <select className='w-100 p-2' value={category} onChange={e => setCategory(e.target.value)}>
+                                                        <option value='chair'>Chair</option>
+                                                        <option value='sofa'>Sofa</option>
+                                                        <option value='mobile'>Mobile</option>
+                                                        <option value='watch'>Watch</option>
+                                                        <option value='wireless'>Wireless</option>
+                                                    </select>
+                                                </Form.Group>
+                                            </div>
+                                            <div>
+                                                <Form.Group className={`${styles.form__group}`}>
+                                                    <Form.Label>Product image</Form.Label>
+                                                    <Form.Control type='file' onChange={e => setImg(e.target.files[0])} />
+                                                </Form.Group>
+                                            </div>
+                                            <button className='primary__btn mt-5' type='submit'>Add Product</button>
+                                        </Form>
+                                    </>
+                            }
+
+                        </Col>
+                    </Row>
+                </Container>
+                <ToastContainer />
+            </section>
         </>
     )
 }
--- src/Dashboard/DashNav/DashNav.js
+++ src/Dashboard/DashNav/DashNav.js
@@ -1,10 +1,82 @@
-import React, { useEffect } from 'react'
-
-
+import React, { useRef, useState, useEffect } from 'react'
+import styles from './DashNav.module.css'
+import { Row, Container } from 'react-bootstrap';
+import logo from "../../assets/images/eco-logo.png"
+import userImg from '../../assets/images/user-icon.png'
+import useAuth from '../../Custom-hooks/useAuth';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 export default function DashNav() {
+    const { currentUser } = useAuth()
+    const admin__nav = [
+        {
+            display: 'Dashboard',
+            path: '/dashboard'
+        },
+        {
+            display: 'All-Products',
+            path: '/dashboard/all-product'
+        },
+        {
+            display: 'Orders',
+            path: '/dashboard/orders'
+        },
+        {
+            display: 'Users',
+            path: '/dashboard/users'
+        },
+    ]
     return (
         <>
-            <div>ddd</div>
+            <header className={`${styles.header}`}>
+                <Container>
+                    <Row>
+                        <div className={`${styles.nav__wrapper}`}>
+                            <div className={`${styles.logo}`}>
+                                <img src={logo} alt='' />
+                                <div>
+                                    <h1>Multimart</h1>
+                                </div>
+                            </div>
+                            <div  >
+                                <input placeholder="Searth the product..." type="text" name="text" className={`${styles.input}`} />
+                            </div>
+                            <div className={`${styles.nav__icons}`}>
+                                <span className={`${styles.fav__icon}`}>
+                                    <NotificationsIcon className={`${styles.i}`} />
+                                    <span className={`${styles.badge}`}>2</span>
+                                </span>
+                                <span className={`${styles.cart__icon}`}>
+                                    <SettingsIcon className={`${styles.i}`} />
+                                    <span className={`${styles.badge}`}>2</span>
+                                </span>
+                                <span>
+                                    <img src={currentUser ? currentUser.photoURL : userImg} alt='' className={`${styles.userImg}`} />
+                                </span>
+                            </div>
+                        </div>
+                    </Row>
+                </Container>
+            </header>
+            <section className={`${styles.admin__menu}`}>
+                <Container>
+                    <Row>
+                        <div className={`${styles.admin__navigation}`}>
+                            <ul className={`${styles.admin__list}`}>
+                                {
+                                    admin__nav.map((item, index) => (
+                                        <li key={index} className={`${styles.admin__item}`}>
+                                            <NavLink to={item.path}>{item.display}</NavLink>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </Row>
+                </Container>
+            </section>
+
         </>
     )
 }
